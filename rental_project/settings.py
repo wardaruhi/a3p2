36fcdbc247416a03a1bcc3a29ba1e7fe02c79304
wardaruhi <60083036+wardaruhi@users.noreply.github.com>
2020-11-18 06:44:12 -0600

    Commit3

--- rental_project/settings.py
+++ rental_project/settings.py
@@ -131,5 +131,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'indxpr@gmail.com'
-EMAIL_HOST_PASSWORD = 'ChickenAteEggsAlphaBetaGamma123'
+EMAIL_HOST_PASSWORD = 'ChickenAteEggs123'
 EMAIL_USE_TLS = True
